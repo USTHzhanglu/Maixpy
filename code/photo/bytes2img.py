a62f113e643d1112dcd4f759467c7a7def9c724c
USTHzhanglu <841891647@qq.com>
2022-05-02 21:56:52 +0800

    new py

--- code/photo/bytes2img.py
+++ code/photo/bytes2img.py
@@ -1,7 +1,7 @@
 # Untitled - By: Lithromantic - 周五 3月 5 2021
 
 import sensor, image, time,lcd
-
+lcd.init(type=5,freq=50*1000*1000,lcd_type=3)
 def Bytes_to_img(imgdir):
     _imgdir=imgdir
     _jpeg_buff={}
@@ -10,5 +10,6 @@ def Bytes_to_img(imgdir):
     _image = image.Image(_jpeg_buff, from_bytes = True)
     return _image
 while(True):
-    img=Bytes_to_img("back_ground.jpg")
+    img=Bytes_to_img("图片1.jpg")
+
     print(img)
